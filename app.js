93f6f56a8c1f1ddd326495206750ff5a7bfba939
chandanp619 <chandanp619@gmail.com>
2018-04-02 07:31:40 +0530

    new structure

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var categories = require('./routes/categories');
 
-
-/* Database Intraction */
-//mongoose.connect('mongodb://localhost:27017/nodeapp');
-mongoose.connect('mongodb://dbacp:dboptions@clusternode-shard-00-00-brgfz.mongodb.net:27017,clusternode-shard-00-01-brgfz.mongodb.net:27017,clusternode-shard-00-02-brgfz.mongodb.net:27017/nodeapp?ssl=true&replicaSet=ClusterNode-shard-0&authSource=admin');
+var db = require('./config/db.js');
 
 var app = express();
 
